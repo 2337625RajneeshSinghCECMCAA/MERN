1df73149c0f57e1194516818d24d48d5407431b9
tictactoe <rajneesh3236@gmail.com>
2025-01-20 19:28:36 +0530

    start commit
    Signed-off-by: tictactoe <rajneesh3236@gmail.com>

---
+++ mernapp/src/components/Carousel.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Carousel() {
+    return (
+        <div>
+
+            <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel" style={{objectFit:"contain !important"}}>
+
+                <div className="carousel-inner " id='carousel'>
+                    <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+                        <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                            <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
+                            <button className="btn text-white bg-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div className="carousel-item active" >
+                        <img src="https://i.insider.com/5bd2234bdde867488579161f?width=700" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    
+                    <div className="carousel-item ">
+                        <img src="https://narcozfoodcartel.com/wp-content/uploads/2023/08/MOMOSSSS.jpg" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://media.istockphoto.com/id/1442417585/photo/person-getting-a-piece-of-cheesy-pepperoni-pizza.jpg?s=612x612&w=0&k=20&c=k60TjxKIOIxJpd4F4yLMVjsniB4W1BpEV4Mi_nb4uJU=" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+
+        </div>
+    )
+}
